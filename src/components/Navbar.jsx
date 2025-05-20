a06148d61e92e5a30b36b129a17241c94a8b4f38
NTNirjana <nafijatabassum79@gmail.com>
2025-05-20 16:40:21 +0600

    web correction done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 
-
 const Navbar = () => {
   const [active, setActive] = useState("home");
 
@@ -72,15 +71,3 @@ const Navbar = () => {
 }
 export default Navbar;
 
- {/*<nav className='w-full  flex items-center justify-center h-fit'>
-      <div className="max-w-screen fixed mx-auto top-4 bg-white rounded-full border-1 border-gray-100 z-50 gap-40">
-        <ul className="flex gap-12 px-10 py-6 justify-center text-base font-semibold text-black">
-        <li><a href="#home" className="nav-hover">Home</a></li>
-        <li><a href="#clothing" className="nav-hover">Clothing</a></li>
-        <li><a href="#construction" className="nav-hover">Construction</a></li>
-        <li><a href="#apparel" className="nav-hover">Apparel</a></li>
-        <li><a href="#agro" className="nav-hover">Agro</a></li>
-        <li><a href="#about" className="nav-hover">About Us</a></li>
-      </ul>
-        </div>
-    </nav>*/}
